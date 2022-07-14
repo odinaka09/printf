470242abb7fa4484708c3726d2bbd6a3509db665
odinaka <odinakanwobodo28@gmail.com>
2022-07-14 13:18:11 -0700

    task 2

--- get_func.c
+++ get_func.c
@@ -12,10 +12,20 @@ int (*get_func(char c))(va_list, flag_t *, mod_t *)
 		{'d', _print_dec},
 		{'i', _print_dec},
 		{'s', _print_str},
-		{'c', _print_char}
+		{'c', _print_char},
+		{'b', _print_binary},
+		{'x', _print_hexa_lower},
+		{'X', _print_hexa_upper},
+		{'o', _print_octal},
+		{'u', _print_unsigned},
+		{'S', _print_specifier},
+		{'p', _print_address},
+		{'%', _print_percent},
+		{'r', _print_reverse},
+		{'R', _print_rot13}
 	};
 	int i;
-	int flags = 4;
+	int flags = 14;
 
 
 	for (i = 0; i < flags; i++)
